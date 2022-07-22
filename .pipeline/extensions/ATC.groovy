30c7900b8fe0053d790bf11ad574b8c8ad56f6f8
Peter Persiel <6087940+peterpersiel@users.noreply.github.com>
2022-07-22 09:27:16 +0200

    Update ATC.groovy

--- .pipeline/extensions/ATC.groovy
+++ .pipeline/extensions/ATC.groovy
@@ -8,7 +8,7 @@ void call(Map params) {
   //execute original stage as defined in the template
   params.originalStage()
   
-  recordIssues tools: [checkStyle(pattern: '**/ATCResults.xml')], qualityGates: [[threshold: 1, type: 'TOTAL', unstable: true], [threshold: 1, type: 'TOTAL_ERROR', unstable: false]]
+  recordIssues tools: [checkStyle(pattern: '**/ATCResults.xml')], qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: false], [threshold: 1, type: 'TOTAL', unstable: true]]
 
   if (currentBuild.result == 'FAILURE') {
     error('Stopping build due to ATC Check quality gate')
